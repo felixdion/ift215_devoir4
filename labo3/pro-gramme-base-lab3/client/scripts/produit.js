97d42273b8805e397569773f179d01a832a7aa48
Felix Dion <felix.dion96@hotmail.com>
2022-11-15 18:15:51 -0500

    Ajouté la section produits
    - Fait l'ajout de produits au panier et l'affichage du nombre de produits dedans le panier

--- labo3/pro-gramme-base-lab3/client/scripts/produit.js
+++ labo3/pro-gramme-base-lab3/client/scripts/produit.js
@@ -26,10 +26,34 @@ function item_to_html(item){
         .append('<li id="quantite">Quantite : ' + item.qte_inventaire +'</li>')
         .append('<li id="categorie">Categorie : ' + item.categorie.nom +'</li>')
         .append('<li id="description">Description : ' + item.description +'</li>')
-        .append('<i class="fa-solid fa-cart-shopping fa-2xl center"></i>');
+        .append('<p class="w-100 display-6 text-center">\n' +
+            '<button type="button" class="btn btn-primary position-relative" onclick="add_item(2)">\n' +
+            '<i class="bi bi-cart-plus"></i>\n' +
+            '</button>\n' +
+            '</p>');
     item_body = $('<div></div>')
         .addClass('card-body')
         .append(' <h1 class="card-title text-center"> $' + item.prix +'</h1>');
     item_card.append(item_head).append(item_body).append(item_detail);
     return $('<div></div>').addClass('col-md-3') .append(item_card);
+}
+
+function add_item(id_item){
+    $.ajax({
+        url: "/clients/"+1+"/panier",
+        method:"POST",
+        data: {"idProduit": id_item, "quantite": 1},
+        beforeSend: function (xhr){
+            xhr.setRequestHeader('Authorization', "Basic "+ "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZENsaWVudCI6MSwicm9sZSI6ImNsaWVudCIsImlhdCI6MTYzNjc1MjI1MywiZXhwIjoxODM2NzUyMjUzfQ.qMcKC0NeuVseNSeGtyaxUvadutNAfzxlhL5LYPsRB8k");
+        },
+        success: function( result ) {
+            console.log(result);
+            let sum = 0;
+            result.items.forEach(sommeQuantite)
+            function sommeQuantite(item) {
+                sum += item.quantite;
+            }
+            $('#item_counter').text(sum)
+        }
+    });
 }
